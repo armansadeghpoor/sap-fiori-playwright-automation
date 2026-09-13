0830f53f208f546f5cb6fd37e0a16696c7a973c7
armansadeghpoor <armansadeghpoor2@gmail.com>
2026-09-13 13:55:37 +0330

    test: verify value change event on datetime field and handle empty tile group

---
+++ TestCases/ثبت درخواست ایشوها/هلدینگ صاد/4156.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:8000/#/login");
+  await page
+    .locator("bsu-layout-control")
+    .filter({ hasText: "نام کاربری*" })
+    .getByRole("textbox")
+    .click();
+  await page
+    .locator("bsu-layout-control")
+    .filter({ hasText: "نام کاربری*" })
+    .getByRole("textbox")
+    .fill("راهبر");
+  await page
+    .locator("bsu-layout-control")
+    .filter({ hasText: "نام کاربری*" })
+    .getByRole("textbox")
+    .press("Tab");
+  await page.getByRole("textbox", { name: "xxxxxxx" }).fill("123");
+  await page.getByRole("textbox", { name: "xxxxxxx" }).press("Enter");
+  await expect(page.getByRole('heading', { name: '4156', exact: true })).toBeHidden();
+});
